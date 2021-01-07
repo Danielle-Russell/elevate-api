5f2abdb51201b94e60490266a91960fe96d32111
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2021-01-07 12:52:10 -0800

    Fixing

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
-   NODE_TLS_REJECT_UNAUTHORIZED=0
-}
+  migrationDirectory: "migrations",
+  driver: "pg",
+  connectionString:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  rejectUnauthorized: false,
+};
