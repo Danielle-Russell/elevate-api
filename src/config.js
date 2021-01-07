a0aeddd7c9fd6f2f7b3b50703b18427ff877e3a3
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2021-01-06 20:13:51 -0800

    added postgrator-cli as a dependency

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/elevatedb",
+    DATABASE_URL: process.env.DATABASE_URL ||  "postgres://yhtsqxrfcpfqwo:6dfb93e021682d30663a991f2bbbe05cb5cac8bdff9ac8d92cc6847ebbd9eefe@ec2-18-211-171-122.compute-1.amazonaws.com:5432/d9411o5lgsegt7",
     JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   }
 
