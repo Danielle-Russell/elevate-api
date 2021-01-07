439cbd1cab08c51f4b6d409ba8990abfc9e49e17
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2021-01-07 12:37:08 -0800

    changed error message for productin debugging

--- src/app.js
+++ src/app.js
@@ -34,12 +34,12 @@ app.use("/api/workouts", workoutRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === "production") {
+  /*if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
-  } else {
+  } else {*/
     console.error(error);
     response = { message: error.message, error };
-  }
+  
   res.status(500).json(response);
 });
 
